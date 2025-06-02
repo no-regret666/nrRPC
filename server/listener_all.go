962cf6abcbe1e24e1a22b7f681268261128391c9
no-regret666 <RangelJara195@gmail.com>
2025-06-02 21:41:44 +0800

    add etcd registries

--- server/listener_all.go
+++ server/listener_all.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"crypto/tls"
 	"errors"
-	quicconn "github.com/marten-seemann/quic-conn"
 	"github.com/xtaci/kcp-go"
 	"net"
 
@@ -25,11 +23,6 @@ func (s *Server) makeListener(network, address string) (ln net.Listener, err err
 			return nil, errors.New("KCP BlockCrypt must be configured in server.Options")
 		}
 		ln, err = kcp.ListenWithOptions(address, s.Options["BlockCrypt"].(kcp.BlockCrypt), 10, 3)
-	case "quic":
-		if s.Options == nil || s.Options["QuicCrypt"] == nil {
-			return nil, errors.New("KCP BlockCrypt must be configured in server.Options")
-		}
-		ln, err = quicconn.Listen("udp", address, s.Options["QuicConfig"].(*tls.Config))
 	default: //tcp,http
 		ln, err = net.Listen(network, address)
 	}
